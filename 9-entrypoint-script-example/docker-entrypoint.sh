ef53f1b9b2f08ea8a518329c37c7edb0ac29d3a9
Mogalipuvvu, Srinivasa <S.Mogalipuvvu@wolterskluwer.com>
2021-01-10 17:14:15 -0500

    Adding Entrypoint and CMD examples

---
+++ 9-entrypoint-script-example/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo ""
+echo  "  HELLO ..!"
+echo ""
+echo "  Our script name is -  " $0
+echo ""
+echo  "  It is coming from our script file ..!"
+echo ""
+
+mkdir temp-dir-creation-from-shell
+echo ""
+
+ls
+echo ""
+
+echo -e " \n Printing all positional parameters: \n   $@ "
+echo ""
+
+
